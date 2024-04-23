47f8d13870f8911bf9a07a30eb42a417915e9e1c
Halle Broadnax <broadnax@chapman.edu>
2024-04-22 22:27:09 -0700

    removing broken link

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,6 @@ export default function Home({ allPostsData }) {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-        <Link href="/posts/first-post">Go to my Profile</Link>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
